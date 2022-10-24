114f2cec6606083cf0db7ecd82518b9e447a19a4
Elsti97 <elsti.shehati@hotmail.it>
2022-10-24 16:28:36 +0200

    Commentato esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// input chiede email
+// Array email varie
+// Controllare che email sia tra le presenti
+// Se presente dare conferma
+// Else dare messaggio di errore
+
+// * DADI
+// numero random da 1 a 6 per pc e player
+// if number player > number pc win
+// if number player < number pc lose
+
